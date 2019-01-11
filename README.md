41036bd4ea4ad0ffe72beb8cc9a0b1f342a21cec
Raul Fiestas <raul.fiestas@trovit.com>
2019-01-11 19:18:03 +0100

    Add Readme and some documentation.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# microbit_level_tool
+micro:bit level tool
+=============
+
+Description
+-------
+BBC micro:bit
+It is a pocket-sized computer 70 times smaller and 18 times faster than the original BBC Micro computers used in schools. It has 25 red LED lights that can flash messages and be used to create games.
+There are two programmable buttons that can be used to control games or pause and skip songs on a playlist.
+
+It has an accelerometer so it can detect motion and knows when you're on the move. The built-in compass knows which direction you're heading in and it can use a low energy Bluetooth connection to interact with other devices and the Internet.
+
+On this script i use the accelerometer as a level tool, it can be used to indicate how parallel (level) a surface is relative to the earth.
+
+
+Usage
+-------
+Clone this repository and flash *main.py* to your micro:bit.
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from microbit import *
 granularity = [20, 100, 500]
 
+
 def position(reading):
+    ''' position, calculate the position on the display '''
+
+    res = 0
     modification = 1
     if reading < 0:
         reading = abs(reading)
@@ -12,23 +16,29 @@ def position(reading):
             break
     return res
 
-def replaceCharPosition(string, poition, char):
-    res = string[0:poition] + char + string[poition+1:]
+
+def replace_char_position(string, position, char):
+    ''' replace_char_position, replace a certain position of string '''
+
+    res = string[0:position] + char + string[position+1:]
     return res
-    
+
+
 def draw(x, y):
+    ''' draw, return a image to print in the display '''
     res = ['0' * 5]*5
     if y-1 > -1:
-        res[y-1] = replaceCharPosition(res[y-1], x, '7')
+        res[y-1] = replace_char_position(res[y-1], x, '7')
     if y+1 < 5:
-        res[y+1] = replaceCharPosition(res[y+1], x, '7')
+        res[y+1] = replace_char_position(res[y+1], x, '7')
     if x-1 > -1:
-        res[y] = replaceCharPosition(res[y], x-1, '7')
+        res[y] = replace_char_position(res[y], x-1, '7')
     if x+1 < 5:
-        res[y] = replaceCharPosition(res[y], x+1, '7')
-    res[y] = replaceCharPosition(res[y], x, '9')
+        res[y] = replace_char_position(res[y], x+1, '7')
+    res[y] = replace_char_position(res[y], x, '9')
     
     return Image(''+':'.join(res))
+
 
 while True:
     x = position(accelerometer.get_x())
